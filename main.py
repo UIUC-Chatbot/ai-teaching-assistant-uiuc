37156e8aeb3d7e2cb50678cf396bd9fd6136223a
Kastan Day <kastanvday@gmail.com>
2022-11-07 04:56:15 +0000

    working DocQuery model, everything looks excellent. Just need to do UX and finetune.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 sys.path.append("../data-generator")
 sys.path.append("../info-retrieval")
 import contriever.contriever_final # Asmita's contriever
@@ -25,6 +26,10 @@ class TA_Pipeline():
     self.user_question = USER_QUESTION
     self.num_answers_generated = NUM_ANSWERS_GENERATED
     self.max_text_length = MAX_TEXT_LENGTH
+    
+    # DocQuery properties (high RAM)
+    self.pipeline = None
+    self.doc = None
     
   def contriever(self, user_question: str = USER_QUESTION, num_answers_generated: int = NUM_ANSWERS_GENERATED):
     ''' Invoke contriever (with reasonable defaults).add()
@@ -83,7 +88,6 @@ class TA_Pipeline():
 
   def re_ranking_ms_marco(self, response_list):
     self._load_reranking_ms_marco()
-    
     assert len([USER_QUESTION] * NUM_ANSWERS_GENERATED ) == len(response_list)
 
     features = rerank_msmarco_tokenizer([USER_QUESTION] * NUM_ANSWERS_GENERATED, response_list,  padding=True, truncation=True, return_tensors="pt")
@@ -100,3 +104,121 @@ class TA_Pipeline():
 
     rerank_msmarco_model = AutoModelForSequenceClassification.from_pretrained('cross-encoder/ms-marco-MiniLM-L-6-v2')
     rerank_msmarco_tokenizer = AutoTokenizer.from_pretrained('cross-encoder/ms-marco-MiniLM-L-6-v2')
+    
+  def doc_query(self, user_question:str = USER_QUESTION, num_answers_generated: int = 1):
+    """ Run DocQuery. Lots of extra dependeicies. 
+    TODO: make it so we can save the 'self.doc' object to disk and load it later.
+    """
+    if not self.pipeline:
+      # load docquery on first use
+      self._load_doc_query()    
+    answer = self.pipeline(question=user_question, **self.doc.context, top_k=num_answers_generated)
+    # todo: this has page numbers, that's nice. 
+    return answer[0]['answer']
+    
+  def _load_doc_query(self):
+    from docquery import document, pipeline
+    self.pipeline = pipeline('document-question-answering')
+    # self.doc = document.load_document("../data-generator/notes/Student_Notes_short.pdf") # faster runtime
+    self.doc = document.load_document("../data-generator/notes/Student_Notes.pdf")
+      
+      
+class DocQuery():
+  """ Class is DEPRICATED """
+  def __init__(self):
+    pass
+  
+  def doc_query_single_question(self,user_question, top_k = 1):
+    answer = self.pipeline(question=user_question, **self.doc.context, top_k=top_k)
+    return answer[0]['answer']
+  
+
+  def query(self, data, top_k: int = 5):
+    """
+    Example return from query
+    [
+      {
+        "questions": " How does the design process for a digital FSM work?",
+        "answers": [
+            {
+                "score": 7.600560638820753e-05,
+                "answer": "we represent all types of information using binary digits, or bits.",
+                "word_ids": [
+                    72,
+                    73,
+                    74,
+                    75,
+                    76,
+                    77,
+                    78,
+                    79,
+                    80,
+                    81,
+                    82
+                ],
+                "page": 2
+            },
+            {
+                "score": 3.47228669852484e-05,
+                "answer": "using binary digits, or bits.",
+                "word_ids": [
+                    78,
+                    79,
+                    80,
+                    81,
+                    82
+                ],
+                "page": 2
+            },
+            {
+                "score": 2.1325537090888247e-05,
+                "answer": "using N-bit 2’s complement,",
+                "word_ids": [
+                    37,
+                    38,
+                    39,
+                    40
+                ],
+                "page": 9
+            }
+        ]
+    },    
+    """
+    from docquery import document, pipeline
+    import json
+    import re
+    import poppler
+    
+    # Call the DocQuery class
+    d = DocQuery()
+    with open('../../data-generator/gpt-3/GPT-3_section_level.json') as f:  # use the questions in section data as query
+      query = json.load(f)
+    # this fn: all_data = d.query(query, 3)
+    data = query
+    
+    # TODO: rework this code to 
+    # input: question (maybe list of contexts?)
+    # output: answer
+    
+    
+    pipeline = pipeline('document-question-answering')
+    doc = document.load_document("../../data-generator/notes/Student_Notes_short.pdf")
+    answer = pipeline(question=q, **doc.context, top_k=top_k)
+    # doc = document.load_document("../Student_Notes.pdf")
+    questions = []
+    for i in range(len(data)):
+      question = re.sub('\nQ.', '', data[i]['questions'])
+      questions.append(question)
+
+    all_data = []
+    for q in questions:
+    # print(q, p(question=q, **doc.context))
+      answer = pipeline(question=q, **doc.context, top_k=top_k)
+      data = {}
+      data['questions'] = q
+      data['answers'] = answer
+      all_data.append(data)
+      
+    
+    
+    return all_data
