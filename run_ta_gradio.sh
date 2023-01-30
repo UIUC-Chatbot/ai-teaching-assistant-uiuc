e8ebc994444e764637734fb5d673dce1987eb2ca
Wentaoy-19 <15726938186@163.com>
2023-01-29 20:06:09 -0600

    all

--- run_ta_gradio.sh
+++ run_ta_gradio.sh
@@ -1,6 +1,9 @@
 export OPENAI_API_KEY=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
-export TRANSFORMERS_CACHE=/home/kastanday/project
+export TRANSFORMERS_CACHE=/mnt/project/chatbotai
+export PINECONE_API_KEY=87823627-c1f4-48fe-9c36-3d19d3dd29bb
+
 python TA_gradio_ux.py
-    
-    
+
+
+    # used by Wentao's custom OPT model
     # --model_weight "/home/wentaoy4/lgm/data/model_weight/opt_finetune_b128_e20_lr5e06.pt" \
