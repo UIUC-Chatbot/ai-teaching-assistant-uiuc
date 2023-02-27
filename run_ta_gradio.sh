736b08aa497cce100261ba9e5758b43a86af5693
Kastan Day <kastanvday@gmail.com>
2023-02-27 15:34:35 -0600

    added new openai api key

--- run_ta_gradio.sh
+++ run_ta_gradio.sh
@@ -1,4 +1,4 @@
-export OPENAI_API_KEY=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
+export OPENAI_API_KEY=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
 export PINECONE_API_KEY=87823627-c1f4-48fe-9c36-3d19d3dd29bb
 export TRANSFORMERS_CACHE=/mnt/project/chatbotai/huggingface_cache/transformers
 export HF_DATASETS_CACHE=/mnt/project/chatbotai/huggingface_cache/datasets
