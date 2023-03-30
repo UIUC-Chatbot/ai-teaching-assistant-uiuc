776d1aaecc7e0459e0c1f49a04c6d6d0cb61aad3
Rohan Salvi <rcsalvi2@illinois.edu>
2023-03-30 18:00:27 -0500

    Modified run_ta_gradio wo keys

--- run_ta_gradio.sh
+++ run_ta_gradio.sh
@@ -1,9 +1,2 @@
-export OPENAI_API_KEY=<YOUR_KEY_HERE>
-export PINECONE_API_KEY=<YOUR_KEY_HERE>
-export GOOGLE_API_KEY=<YOUR_KEY_HERE>
-export SEARCH_ENGINE_ID=<YOUR_KEY_HERE>
-# use shared cache to reduce model duplication
-export TRANSFORMERS_CACHE=/mnt/project/chatbotai/huggingface_cache/transformers
-export HF_DATASETS_CACHE=/mnt/project/chatbotai/huggingface_cache/datasets
 
 python TA_gradio_ux.py
